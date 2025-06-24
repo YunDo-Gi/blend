b0fda0184c1a0c1c24c6328fbeef6a276b17bada
yundogi <lvbhkwak@gmail.com>
2025-06-24 17:53:47 +0900

    feat: 테마 변경 시 로고 색상 변경 추가

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -5,9 +5,12 @@ import styled from 'styled-components';
 const MorphShape = styled.div`
   width: 30px;
   height: 30px;
-  background: var(--foreground);
+  background: var(--color-foreground);
   position: relative;
-  transition: all 0.8s cubic-bezier(0.68, -0.55, 0.265, 1.55);
+  transition:
+    transform 0.8s cubic-bezier(0.68, -0.55, 0.265, 1.55),
+    border-radius 0.8s cubic-bezier(0.68, -0.55, 0.265, 1.55),
+    background-color var(--animation-duration-toggle-theme) var(--easing-toggle-theme);
   border-radius: 0;
 
   &::before {
@@ -15,12 +18,17 @@ const MorphShape = styled.div`
     position: absolute;
     width: 12px;
     height: 12px;
-    background: var(--background);
+    background: var(--color-background);
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
     border-radius: 50%;
-    transition: all 0.8s cubic-bezier(0.68, -0.55, 0.265, 1.55);
+    transition:
+      transform 0.8s cubic-bezier(0.68, -0.55, 0.265, 1.55),
+      border-radius 0.8s cubic-bezier(0.68, -0.55, 0.265, 1.55),
+      width 0.8s cubic-bezier(0.68, -0.55, 0.265, 1.55),
+      height 0.8s cubic-bezier(0.68, -0.55, 0.265, 1.55),
+      background-color var(--animation-duration-toggle-theme) var(--easing-toggle-theme);
   }
 `;
 
