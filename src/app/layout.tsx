d7383c58dc08645541687b9f60fa26f321d2d26e
yundogi <lvbhkwak@gmail.com>
2025-06-24 17:46:02 +0900

    design: 커스텀 스타일 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { ThemeProvider } from '@/components/providers/theme-provider';
+import StyledComponentsRegistry from '@/lib/registry';
+import Header from '@/layout/header';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -26,13 +28,16 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased transition-colors duration-700 ease-in-out`}
+        className={`${geistSans.variable} ${geistMono.variable} bg-background text-foreground antialiased transition-colors duration-(--animation-duration-toggle-theme) ease-(--easing-toggle-theme)`}
       >
-        <ThemeProvider attribute="class">
-          <div className="flex min-h-screen flex-col">
-            <main className="flex-1">{children}</main>
-          </div>
-        </ThemeProvider>
+        <StyledComponentsRegistry>
+          <ThemeProvider attribute="class">
+            <Header />
+            <div className="flex min-h-screen flex-col">
+              <main className="flex-1">{children}</main>
+            </div>
+          </ThemeProvider>
+        </StyledComponentsRegistry>
       </body>
     </html>
   );
--- src/components/lamp-toggle.tsx
+++ src/components/lamp-toggle.tsx
@@ -13,9 +13,11 @@ export default function ClickableLampIcon() {
         className="flex flex-col items-center p-2"
         aria-label="램프 아이콘 클릭"
       >
-        <LampIcon className={`h-fit w-32 p-0 transition-colors duration-700 ease-in-out`} />
+        <LampIcon
+          className={`h-fit w-32 p-0 transition-colors duration-(--animation-duration-toggle-theme) ease-(--easing-toggle-theme)`}
+        />
         <div
-          className={`from-primary relative h-4 w-12 bg-gradient-to-b to-transparent ${theme === 'dark' ? 'opacity-50' : 'opacity-0'} transition-opacity delay-300 duration-700 ease-in-out`}
+          className={`from-primary relative h-4 w-12 bg-gradient-to-b to-transparent ${theme === 'dark' ? 'opacity-50' : 'opacity-0'} transition-opacity delay-300 duration-(--animation-duration-toggle-theme) ease-(--easing-toggle-theme)`}
           style={{ clipPath: 'polygon(25% 0%, 75% 0%, 100% 100%, 0% 100%)' }}
         ></div>
       </button>
